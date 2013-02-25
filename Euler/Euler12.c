483d39b9b87b800ee402f1232bb1b34bd5033595
Brent Erickson <brent2737@gmail.com>
2013-02-25 12:30:15 -0700

    Optimized solution for Problem 12: Highly Divisible Triangle Number

--- Euler/Euler12.c
+++ Euler/Euler12.c
@@ -3,10 +3,20 @@
 
 //Returns the first triangle number with over 500 divisors
 int Euler12(){
-	int iteration, divisors = 0, testDivisor;
-	int num = 0;
-	for (iteration = 1; divisors < 500; iteration++){
+	int iteration = 0, divisors = 0, testDivisor;
+	int num = 0, minNum = (int) pow(2.0, 25);
+	//The smallest nummber we could possibly find is 2^25
+	//We can ignore all numbers  below that but we must
+	//still find the iteration number inorder to compute
+	//the triange number
+	while ( num < minNum){
+		iteration++;
+		num += iteration;
+	}
+
+	while (divisors < 500){
 		divisors = 0;
+		iteration++;
 		num += iteration;
 		for (testDivisor = 1; testDivisor < sqrt((double) num); testDivisor++){
 			if (num % testDivisor == 0){
